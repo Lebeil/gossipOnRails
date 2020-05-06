4eddfdb3ecfe37412e32b6c440b6b7d7097f25e5
Lebeil <liedel75@gmail.com>
2020-05-06 17:58:21 +0200

    Start: full CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # '/'
+  root 'gossips#index'
+
   resources :gossips
-  # root 'front#gossips'
   # get 'gossips/:id', to: 'front#gossip', as: 'gossip'
   get 'team', to: 'front#team'
   get 'contact', to: 'front#contact'
