0b5b7e191152d41ce1f2a22474ba1b43237cfdef
Lebeil <liedel75@gmail.com>
2020-05-08 10:11:36 +0200

    maj

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(params.require(:user).permit(:email, :password, :first_name, :last_name, :city_id))
-    session[:user_id] = @user.id
-    redirect_to gossips_path
+    @user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], age: params[:age], description: params[:description], password: params[:password], city: City.all.sample)
+
+    if @user.save
+      puts "Operation success"
+      flash[:success] = "Création d'un nouveau profil réussie"
+      redirect_to new_session_path
+    else
+      puts "Failure"
+      flash[:failure] = "Formulaire invalide"
+      render :new
+    end
   end
 
   def show
